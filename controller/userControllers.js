6c3b6438a033abf9eb7cde88acb1c259090bd88e
Umka-dev <design.tvin@gmail.com>
2025-03-18 22:42:47 +0100

    add the solution of the task

--- controller/userControllers.js
+++ controller/userControllers.js
@@ -1,5 +1,27 @@
 const homePage = (req, res) => {
-  res.render('homepage');
+  const dataObj = [
+    {
+      name: 'Michael Choi',
+      createdAt: '18-03-2025',
+      message:
+        'This is my message    This is my message This is my message This is my messageThis is my message',
+    },
+    {
+      name: 'Luisa Hloe',
+      createdAt: '15-03-2025',
+      message:
+        'This is my message    This is my message This is my message This is my messageThis is my message',
+    },
+    {
+      name: 'John Doe',
+      createdAt: '05-02-2025',
+      message:
+        'This is my message    This is my message This is my message This is my messageThis is my message',
+    },
+  ];
+  res.render('homepage', {
+    postList: dataObj,
+  });
 };
 
 const notFoundPage = (req, res) => {
--- server.js
+++ server.js
@@ -5,10 +5,4 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(routes);
-
 app.listen(3000, () => console.log('Web Server running on port 3000'));
-
-// $(document).ready(() => {
-//   // Get current year for the footer
-//   $('#currentYear').text(new Date().getFullYear());
-// });
