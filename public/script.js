42efceaf867073af48a43e698625c5d72b86286c
Umka-dev <design.tvin@gmail.com>
2025-03-19 11:01:56 +0100

    add an actual year to the footer

---
+++ public/script.js
@@ -0,0 +1,4 @@
+$(document).ready(() => {
+  $('#currentYear').text(new Date().getFullYear());
+  console.log('Year updated!');
+});
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./config/routes');
 const app = express();
 
+app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 
 app.use(routes);
