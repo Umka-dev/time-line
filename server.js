58f8d72b06157bc3e2817c1b929b05594e0e6ad0
Umka-dev <design.tvin@gmail.com>
2025-03-19 11:18:30 +0100

    add a const port for better readability

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const routes = require('./config/routes');
 const app = express();
+const port = 3000;
 
 app.use('/public', express.static('public'));
 app.set('view engine', 'ejs');
 
 app.use(routes);
-app.listen(3000, () => console.log('Web Server running on port 3000'));
+app.listen(port, () =>
+  console.log(`Server is running on http://localhost:${port}`)
+);
