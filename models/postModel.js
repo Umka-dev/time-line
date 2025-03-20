bd82557e6be2392202ea5acd9c15b6a28bab56b1
Umka-dev <design.tvin@gmail.com>
2025-03-20 12:17:48 +0100

    add partials

--- models/postModel.js
+++ models/postModel.js
@@ -2,17 +2,14 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
   name: {
-    // name: 'Michael Choi'
     type: String,
     required: true,
   },
   createdAt: {
-    // createdAt: '18-03-2025'
     type: String,
     required: true,
   },
   message: {
-    // message: 'some text'
     type: String,
     required: true,
   },
